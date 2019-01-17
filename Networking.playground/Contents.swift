1260ea89c97d15c3d0fbcd672d79935e9e85cc6e
Ben <BeshBashBosh@users.noreply.github.com>
2019-01-17 17:19:40 +0000

    Network requests as URLSessionDataTask

--- Networking.playground/Contents.swift
+++ Networking.playground/Contents.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+// Enable async execution support for playground
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
 // Schemes
 enum URLSchemes: String {
     case http, https
@@ -17,7 +21,34 @@ func constructURLComponentsFrom(scheme: URLSchemes, host: String, path: String) 
 let urlComponents = constructURLComponentsFrom(scheme: .https, 
                                                host: "api.github.com",
                                                path: "/users/beshbashbosh")
-print(urlComponents.url)
+print(urlComponents.url ?? "")
+
+// Network Request with URLSession
+func createDataTaskFrom(_ components: URLComponents, with session: URLSession) -> URLSessionDataTask {
+    guard let url = urlComponents.url else {
+        preconditionFailure("Failed to construct url")
+    }
+    
+    let task = session.dataTask(with: url) {
+        data, response, error in
+        // data - the data in bytes returned from request (nil if error)
+        // response - basic response info such as MIME and data encoding
+        // can be typecast as HTTPURLResponse to get HTTP-specific info (e.g. status code)
+        // error - error if request failed, nil if not
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        let dataAsString = String(data: data!, encoding: .utf8)
+        print(dataAsString)
+    }
+    return task
+}
+
+let task = createDataTaskFrom(urlComponents, with: URLSession())
+task.resume
+
 
 
 
