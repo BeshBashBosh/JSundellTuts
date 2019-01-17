79f3a2e7a144d6f3a62c841fc2c9ce4dd9fd1f29
Ben <BeshBashBosh@users.noreply.github.com>
2019-01-17 16:30:37 +0000

    Constructing url from components and URLComponents

--- Networking.playground/Contents.swift
+++ Networking.playground/Contents.swift
@@ -1,1 +1,24 @@
+import Foundation
+
+// Schemes
+enum URLSchemes: String {
+    case http, https
+}
+
+// Constructing URLs
+func constructURLComponentsFrom(scheme: URLSchemes, host: String, path: String) -> URLComponents {
+    var components = URLComponents()
+    components.scheme = scheme.rawValue
+    components.host = host
+    components.path = path
+    return components
+}
+
+let urlComponents = constructURLComponentsFrom(scheme: .https, 
+                                               host: "api.github.com",
+                                               path: "/users/beshbashbosh")
+print(urlComponents.url)
+
+
+
 
