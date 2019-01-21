3a4eb8f483c6d14158378dfeab4eca3d11695163
Ben <BeshBashBosh@users.noreply.github.com>
2019-01-21 15:23:29 +0000

    Added a typed error result
    This allows us to restrict the error types and handle respectively.

--- Result.playground/Contents.swift
+++ Result.playground/Contents.swift
@@ -7,26 +7,85 @@ import Foundation
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+// MARK: - Result Type
 enum Result<Value> {
     case success(Value)
     case failure(Error)
 }
 
-typealias StringResult = (Result<String>) -> Void
-
-func load(then handler: @escaping StringResult) {
-    let successString = "Woo!"
-    handler(.success(successString))
+enum TestError: Error {
+    case noResult
+    case sorryICantDoThatDave
 }
 
-load { (result) in
+// MARK: - Result Example
+typealias StringResult = Result<String>
+typealias StringResultHandler = (StringResult) -> Void
+
+func testResult(asError: Bool, with handler: @escaping StringResultHandler) {
+    if asError {
+        handler(.failure(TestError.noResult))
+        return
+    }
+    handler(.success("Woo!"))
+}
+
+func process(_ result: StringResult) {
     switch result {
-        case .success(let data):
+    case .success(let data):
         // handle success data
         print(data)
-        case .failure(let error):
+    case .failure(let error):
         // handle error
-            print(error.localizedDescription)
+        print(error)
     }
 }
 
+testResult(asError: false) { (result) in
+    process(result)
+}
+
+testResult(asError: true) { (result) in
+    process(result)
+}
+
+// MARK: - Typed Errors
+/*
+ Using the below we have to specify the type of error a failure will kick back
+ */
+enum TypedErrorResult<Value, Error: Swift.Error> {
+    case success(Value)
+    case failure(Error)
+}
+
+// Example
+typealias TestErrorResult = TypedErrorResult<String, TestError>
+typealias TypedErrorHandler = (TypedErrorResult<String, TestError>) -> Void
+  
+ func testTypedErrorResult(asError: Bool, with handler: @escaping TypedErrorHandler) {
+    if asError {
+        handler(.failure(TestError.sorryICantDoThatDave))
+        return
+    }
+    handler(.success("Woo!"))
+  }
+
+func process(_ result: TestErrorResult) {
+    switch result {
+        case .success(let data): print(data)
+        case .failure(let error): 
+            switch error {
+                case .noResult: print("Nada")
+                case .sorryICantDoThatDave: print("DAAAAAAA DAAAAAA DA DAAAAAAAA! DUM DUM DUM DUM")
+            }
+    }
+}
+
+testTypedErrorResult(asError: false) { (result) in
+    process(result)
+}
+
+testTypedErrorResult(asError: true) { (result) in
+    process(result)
+}
+  
