424eebda1dfbb974334af1f8e9bd165fabb27e25
Ben <BeshBashBosh@users.noreply.github.com>
2019-01-22 19:13:43 +0000

    Added Result type and extension to FuturePromise

--- FuturePromise.playground/Contents.swift
+++ FuturePromise.playground/Contents.swift
@@ -4,3 +4,34 @@
  */
 
 import Foundation
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// What is a Future and Promise?
+/*
+ - Promise - something you make to someone else (gets contructed)
+ - Future - In the future you choose to honor (resolve) that promise, or reject it.
+ 
+ Futures and Promises are very useful for organising logic of async code
+ */
+
+enum Result<Value, Error: Swift.Error> {
+    case success(Value)
+    case failure(Error)
+}
+
+extension Result {
+    func resolve() throws -> Value {
+        switch self {
+            case .success(let value): return value
+            case .failure(let error): throw error
+        }
+    }
+}
+
+let session = URLSession()
+
+//  
+//  class UserLoader {
+//      typealias Handler = (Result<)
+//  }
